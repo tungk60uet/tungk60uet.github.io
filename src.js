8e356071f7ae92e27c31dcd6cb0d57a017c0f407
tungk60uet <tungk60uet@gmail.com>
2019-06-22 16:59:56 +0700

    fixx

--- src.js
+++ src.js
@@ -10,10 +10,7 @@ $(window).on('load', function() {
     $('.font-weight-light').click(function(){
         isCheat=true;
     });
-    $('.subscribeModal-lg').on('hidden.bs.modal', function (e) {
-        $('.ketqua').prepend("<p class='font-weight-bold' style='font-size: 1.5em; color: darkcyan;'>Tổ thứ "+arr[i]+" đi trực thứ "+(i+1)+"</p>");
-        i++;
-    });
+    
     $(".random-btn" ).click(function() {
         if(arr.length===0){
             n=parseInt($("#sophantu").val())
